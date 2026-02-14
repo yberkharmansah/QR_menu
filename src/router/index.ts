1ede3989b0a1231ec7ffa90135d124e7a0e7613e
yberkharmansah <yberkharmansah@gmail.com>
2026-02-14 07:02:58 +0300

    chore: add github pages deployment workflow

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import CategoriesView from "../views/CategoriesView.vue";
 import ProductsView from "../views/ProductsView.vue";
 import ProductDetailView from "../views/ProductDetailView.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: "/", name: "home", component: HomeView },
     { path: "/categories", name: "categories", component: CategoriesView },
